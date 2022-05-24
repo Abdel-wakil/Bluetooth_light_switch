3df9d4ee52e0b4e7773352f5a114bec40be7047c
Abdel-wakil <84014968+Abdel-wakil@users.noreply.github.com>
2022-05-24 17:22:01 +0100

    Add files via upload

---
+++ bluetooth_test_for_app/bluetooth_test_for_app.ino
@@ -0,0 +1,39 @@
+#include <SoftwareSerial.h>
+
+String BT_msg;
+String USB_msg;
+
+
+
+SoftwareSerial BTSerial(2,3);//  RX--10 et TX--11
+ 
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("enter command : ");
+  BTSerial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (BTSerial.available()){
+    BT_msg = BTSerial.readStringUntil('\n');
+    Serial.println(BT_msg);
+  }
+  
+    
+    while (Serial.available()){
+      USB_msg = Serial.readString();
+      BTSerial.println(USB_msg);
+    
+     
+    }
+
+  delay(1000);    
+
+    
+}
